745619c911e7bc6d4b86712cd76b07bf61e500d1
crackcomm <crackcomm@gmail.com>
2015-11-17 00:21:43 +0100

    Domains -list option

--- ovh/cmd/cmd_domains_list.go
+++ ovh/cmd/cmd_domains_list.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -13,11 +14,25 @@ import (
 var cmdDomainsList = cli.Command{
 	Name:  "list",
 	Usage: "lists domains",
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "list",
+			Usage: "print list of domains instead of table",
+		},
+	},
 	Action: func(c *cli.Context) {
 		domains, err := client(c).Domains.List(context.Background())
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		if c.Bool("list") {
+			for _, domain := range domains {
+				fmt.Println(domain)
+			}
+			return
+		}
+
 		table := tablewriter.NewWriter(os.Stdout)
 		table.SetHeader([]string{"Domain"})
 		for _, domain := range domains {
